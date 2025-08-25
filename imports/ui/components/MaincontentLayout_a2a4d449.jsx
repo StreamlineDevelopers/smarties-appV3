ad872579d203ed36dde1ce7c55bc7ddd5aaecfd4
Jomel <jomel.p@txtmequick.com>
2025-08-25 15:01:22 -0400

    Update MaincontentLayout to reflect call start time dynamically
    - Changed the displayed text in ConvodividercontentItem to show 'Call Started' with the formatted timestamp.
    - Commented out the user typing div for potential future use, maintaining code clarity.
    - Adjusted the structure of the reply row for better interaction handling.
    
    Signed-off-by: jom <tmq.jomel@gmail.com>

--- imports/ui/components/MaincontentLayout_a2a4d449.jsx
+++ imports/ui/components/MaincontentLayout_a2a4d449.jsx
@@ -978,7 +978,7 @@ const MaincontentLayout_a2a4d449 = ({ }) => {
                                   <div className={'convo-divider-line'}></div>
                                   <ConvodividercontentItem
                                     src={'images/smarties-avatar-icon-endcall.svg'}
-                                    divText={'Call Ended • 10:35 AM'}
+                                    divText={`Call Started • ${moment(data.timestamp).format('h:mm A')}`}
                                   />
                                   <div className={'convo-divider-line'}></div>
                                 </div>
@@ -1266,7 +1266,7 @@ const MaincontentLayout_a2a4d449 = ({ }) => {
                         </div>
                       </div> */}
                     </div>
-                    <div className={'user-typing-div'}>
+                    {/* <div className={'user-typing-div'}>
                       <div className={'usertyping-avatar'}>
                         <img
                           loading={'lazy'}
@@ -1277,7 +1277,7 @@ const MaincontentLayout_a2a4d449 = ({ }) => {
                         />
                       </div>
                       <div>{'John Smith is typing...'}</div>
-                    </div>
+                    </div> */}
                     <div className={'reply-row'}
                       style={{ pointerEvents: isSmartiesAssistantToggled ? 'none' : 'auto', opacity: isSmartiesAssistantToggled ? 0.5 : 1 }}>
                       <div className={'reply-row-aisuggestion-row'} style={{ display: isSmartiesAssistantToggled ? 'none' : 'block' }}>
